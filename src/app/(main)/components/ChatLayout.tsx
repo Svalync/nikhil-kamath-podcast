c6d57d8b91a16f3810555c0808abdf4364dedb7a
Uday <rahul.uday07@gmail.com>
2025-02-14 14:27:43 +0530

    Added nitin kamath podcast

---
+++ src/app/(main)/components/ChatLayout.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useState } from "react";
+
+export default function ChatLayout() {
+  const [loaded, setLoaded] = useState(false);
+
+  return (
+    <div className="container mx-auto h-screen bg-gray-100 flex items-center justify-center">
+      {!loaded && <p>Loading chatbot...</p>}
+      <iframe
+        height="100%"
+        width="100%"
+        src="http://svalync.com/widget_chatbot/fd11c5e8-c7d9-4d7e-bdf2-10f7010d02cf"
+        onLoad={() => setLoaded(true)}
+        style={{
+          display: loaded ? "block" : "none",
+          opacity: loaded ? 1 : 0,
+          transition: "opacity 0.2s ease-in-out",
+        }}
+      ></iframe>
+    </div>
+  );
+}
